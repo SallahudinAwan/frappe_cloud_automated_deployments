ac9eae4f587b7071e37f06237c43639ef1858050
SallahudinAwan <sallahudinawan8@gmail.com>
2025-09-10 09:29:20 +0500

    git commit -m "first commit"

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+GOOGLE_CHAT_WEBHOOK = "https://chat.googleapis.com/v1/spaces/AAQA4gwdpHQ/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=LImmQc57oGth_ybsOB3cw4PQuuaNr2NOi9W-sdkNISs"
+
+@app.route("/", methods=["GET"])
+def home():
+    return "✅ Frappe Cloud → Google Chat Middleware is running!"
+
+@app.route("/frappe-cloud-webhook", methods=["POST"])
+def handle_webhook():
+    data = request.json
+    message = f"""
+📢 Event: {data.get('event')}
+🌐 Site: {data.get('site')}
+🕒 Time: {data.get('time')}
+"""
+    requests.post(GOOGLE_CHAT_WEBHOOK, json={"text": message})
+    return "ok", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
